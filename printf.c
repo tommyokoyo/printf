b8afc799f01b730a84c67131ffe84eaf76acd8c3
theOnyangos <otienodennis29@gmail.com>
2022-10-19 04:32:38 -0700

    printf file creation

---
+++ printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _printf - A function.
+ * @format: The constant string literal that will be passed
+ * @...: Represent the number of arguments that will be passed.
+ *
+ * Description: The function takes in different types
+ * or arguments and prints them out to
+ * standard output.
+ *
+ * Return: an integer on success.
+ */
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
